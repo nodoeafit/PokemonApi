83024214577805f2feb5a7ee0527e0b67b6a310b
appitoriadev <contact@appitoria.dev>
2024-12-04 20:09:42 -0500

    Refactor HTML structure and improve JavaScript variable naming for better readability

--- index.js
+++ index.js
@@ -6,16 +6,25 @@
 // 🤓 Pista: revisa el método document.querySelector()
 
 // Selectores para el Pokemon 1
-
-
+let pokemon1img = document.querySelector(".pokemon-1__img");
+let pokemon1hp = document.querySelector(".pokemon-1__hp");
+let pokemon1name = document.querySelector(".pokemon-1__name");
+let pokemon1attack = document.querySelector(".pokemon-1__attack");
+let pokemon1defense = document.querySelector(".pokemon-1__defense");
+let pokemon1type = document.querySelector(".pokemon-1__type");
 // Selectores para el Pokemon 2
+let pokemon2img = document.querySelector(".pokemon-2__img");
+let pokemon2hp = document.querySelector(".pokemon-2__hp");
+let pokemon2name = document.querySelector(".pokemon-2__name");
+let pokemon2attack = document.querySelector(".pokemon-2__attack");
+let pokemon2defense = document.querySelector(".pokemon-2__defense");
+let pokemon2type = document.querySelector(".pokemon-2__type");
 
 
 // 2️⃣. Miremos ahora la API de Pokemon :)
 // - Haz un llamado a la URL https://pokeapi.co/api/v2/pokemon/ y analiza cómo devuelve su respuesta
 // La API retorna un pokemon https://pokeapi.co/api/v2/pokemon/{ID} si se provee un ID al final.
-// 🤓 Pista: Para enfrentar 2 pokemones aleatores, necesitamos hacer 2 llamados a la API con 2 n´¨úmeros aleatorios entre el 1 y el 900
-
+// 🤓 Pista: Para enfrentar 2 pokemones aleatores, necesitamos hacer 2 llamados a la API con 2 números aleatorios entre el 1 y el 900
 
 // 3️⃣ - Crear una función que genere un número random entre 1 y 900.
 // Puedes usar esta: 👩🏻‍💻
@@ -28,16 +37,30 @@
 
 // 🤓 Pista: algo como ... const poke1ID = getRandomNumber(1, 900);
 
+const obtenerNumeroAlAzar = (nMin, nMax) => {
+  return Math.floor(Math.random() * (nMax - nMin + 1) + nMin);
+}
+
+let pokemonID1 = obtenerNumeroAlAzar(1, 900);
+let pokemonID2 = obtenerNumeroAlAzar(1, 900);
+
+
 // 5️⃣ - Crear una función para traer (fetch) data de la API
 // Dale una mirada a la función fetch -> https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
 // Recuerda la URL de la API https://pokeapi.co/api/v2/pokemon/${pokeID}
 
 //Puedes usar esta: 👩🏻‍💻
-/* const getPokemon = async (pokeID) => {
-  const response = await fetch(` https://pokeapi.co/api/v2/pokemon/${pokeID}`);
+
+const traerPokemon = async (pokeID) => {
+  let url = `https://pokeapi.co/api/v2/pokemon/${pokeID}`;
+  console.log(url);
+  const response = await fetch(url);
   const data = await response.json();
   return data;
-}; */
+}; 
+
+// Open/Closed principle - Single Responsibility Principle
+
 
 // 6️⃣ - Vamos a crear los pokemons en la función createPokemons.
 // Primero Haz varias pruebas a las API para examinar bien qué devuelve, esa data
@@ -51,37 +74,37 @@
 
 // 🎁 Bonus! - Vamos a crear la función fightPokemons que permitirá que los pokemons interactúen y peleen
 
-  // 1. Seleccionar los datos que ahora tenemos en el HTML y que trajimos desde la API: para ambos pokemon: HP, attack, defense y name.
+// 1. Seleccionar los datos que ahora tenemos en el HTML y que trajimos desde la API: para ambos pokemon: HP, attack, defense y name.
 
 
-  // 2. Crear una función que calcule el daño hecho a cada pokemon. Necesitamos el poder del atacante y la defensa y los HP del que defiende
-  // - Calcular el daño restando el ataque de la defensa, con esto definimos si el pokemon sufrió daño.
-  // - Calcular los nuevos HP: Si la defensa es menor a 0, significa que el ataque logró perforarla e hizo daño, en este caso vamos a restar el daño de los HP, si no, la HP debe quedar igual pues no hubo da˜ño
-  // En esta función vamos a devolver la nueva HP del pokemon atacado y además el da˜ñó que sufrió. - Luego vamos a necesitar estos datos -
+// 2. Crear una función que calcule el daño hecho a cada pokemon. Necesitamos el poder del atacante y la defensa y los HP del que defiende
+// - Calcular el daño restando el ataque de la defensa, con esto definimos si el pokemon sufrió daño.
+// - Calcular los nuevos HP: Si la defensa es menor a 0, significa que el ataque logró perforarla e hizo daño, en este caso vamos a restar el daño de los HP, si no, la HP debe quedar igual pues no hubo da˜ño
+// En esta función vamos a devolver la nueva HP del pokemon atacado y además el da˜ñó que sufrió. - Luego vamos a necesitar estos datos -
 
 
-  // 3. Narrar la batalla ;). Para esto vamos a usar el elemento modal__text, aquí vamos a ir llenando su innerHTML.
-  // Empecemos con el Pokemon 1.
-
-
-
-  // Ahora calculemos el daño que le hizo a pokemon2 y cuánta vida le queda, usemos la función de calcular daño
+// 3. Narrar la batalla ;). Para esto vamos a usar el elemento modal__text, aquí vamos a ir llenando su innerHTML.
+// Empecemos con el Pokemon 1.
 
 
 
-  // Vamos a narrar qué pasó en este ataque.Si el pokemon 1 tiene un ataque mayor a la denfesa del pokemon 2, debemos narrar que logra perforar su defensa
-  // y describir cuánto daño recibió y cuáles son ahora sus puntos de vida
-  // Si el ataque del pokemon 1 no es mayor que la denfesa del pokemon 2, significa que no logra perforarla y por lo tanto no hay daño.
+// Ahora calculemos el daño que le hizo a pokemon2 y cuánta vida le queda, usemos la función de calcular daño
 
 
-  // Ahora el Pokemon2, mismo procedimiento.
+
+// Vamos a narrar qué pasó en este ataque.Si el pokemon 1 tiene un ataque mayor a la denfesa del pokemon 2, debemos narrar que logra perforar su defensa
+// y describir cuánto daño recibió y cuáles son ahora sus puntos de vida
+// Si el ataque del pokemon 1 no es mayor que la denfesa del pokemon 2, significa que no logra perforarla y por lo tanto no hay daño.
 
 
-  // Definamos el ganador que sería el más daño haya hecho al otro pokemon.
-  // Recordemos que los puntos de daño son negativos!!
-  // - Si el daño recibido por pokemon 2 es menor al de pokemon 1, (es decir un mayor número negativo), significa que pokemon 1 hizo más daño, por lo tanto es el gandor!
-  // - En caso de que sea menor el daño de pokemon 1, significa que pokemon 2 es el gandor
-  // - El último caso posible es que ambos hayan recibido el mismo daño, en ese caso sería un empate.
+// Ahora el Pokemon2, mismo procedimiento.
+
+
+// Definamos el ganador que sería el más daño haya hecho al otro pokemon.
+// Recordemos que los puntos de daño son negativos!!
+// - Si el daño recibido por pokemon 2 es menor al de pokemon 1, (es decir un mayor número negativo), significa que pokemon 1 hizo más daño, por lo tanto es el gandor!
+// - En caso de que sea menor el daño de pokemon 1, significa que pokemon 2 es el gandor
+// - El último caso posible es que ambos hayan recibido el mismo daño, en ese caso sería un empate.
 
 
 
