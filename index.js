39b88f81aef300bc3c4ef9e857030b64b49adaaa
appitoriadev <contact@appitoria.dev>
2025-01-17 19:22:17 -0500

    Fix script path in HTML and improve variable naming in JavaScript for better clarity

--- index.js
+++ index.js
@@ -1,76 +1,83 @@
 // Ahora que tenemos nuestro HTML y CSS, es hora de darle vida con JavaScript <3
 
-// 1️⃣. Seleccionar los elementos HTML que vamos a utilizar:
+// 1. Seleccionar los elementos HTML que vamos a utilizar:
 // - Imagen de los pokemon
 // - Stats de cada uno
-// 🤓 Pista: revisa el método document.querySelector()
+// Pista: revisa el método document.querySelector()
 
-// Selectores para el Pokemon 1
-let pokemon1img = document.querySelector(".pokemon-1__img");
-let pokemon1hp = document.querySelector(".pokemon-1__hp");
-let pokemon1name = document.querySelector(".pokemon-1__name");
-let pokemon1attack = document.querySelector(".pokemon-1__attack");
-let pokemon1defense = document.querySelector(".pokemon-1__defense");
-let pokemon1type = document.querySelector(".pokemon-1__type");
-// Selectores para el Pokemon 2
-let pokemon2img = document.querySelector(".pokemon-2__img");
-let pokemon2hp = document.querySelector(".pokemon-2__hp");
-let pokemon2name = document.querySelector(".pokemon-2__name");
-let pokemon2attack = document.querySelector(".pokemon-2__attack");
-let pokemon2defense = document.querySelector(".pokemon-2__defense");
-let pokemon2type = document.querySelector(".pokemon-2__type");
+// Pokemon 1
+let poke1ImgElement = document.querySelector(".pokemon-1__img");
+let poke1HpElement = document.querySelector(".pokemon-1__hp");
+let poke1NameElement = document.querySelector(".pokemon-1__name");
+let poke1AttackElement = document.querySelector(".pokemon-1__attack");
+let poke1DefenseElement = document.querySelector(".pokemon-1__defense");
+let poke1TypeElement = document.querySelector(".pokemon-1__type");
 
+// Pokemon 2
+let poke2ImgElement = document.querySelector(".pokemon-2__img");
+let poke2HpElement = document.querySelector(".pokemon-2__hp");
+let poke2NameElement = document.querySelector(".pokemon-2__name");
+let poke2AttackElement = document.querySelector(".pokemon-2__attack");
+let poke2DefenseElement = document.querySelector(".pokemon-2__defense");
+let poke2TypeElement = document.querySelector(".pokemon-2__type");
 
-// 2️⃣. Miremos ahora la API de Pokemon :)
+// 2 - Analizar la API de Pokemon :)
 // - Haz un llamado a la URL https://pokeapi.co/api/v2/pokemon/ y analiza cómo devuelve su respuesta
-// La API retorna un pokemon https://pokeapi.co/api/v2/pokemon/{ID} si se provee un ID al final.
-// 🤓 Pista: Para enfrentar 2 pokemones aleatores, necesitamos hacer 2 llamados a la API con 2 números aleatorios entre el 1 y el 900
+// Al API retorna un pokemon https://pokeapi.co/api/v2/pokemon/{ID} si se provee un ID al final.
+// Para enfrentar 2 pokemones aleatores, necesitamos hacer 2 llamados a la API con 2 n´¨úmeros aleatorios entre el 1 y el 900
 
-// 3️⃣ - Crear una función que genere un número random entre 1 y 900.
-// Puedes usar esta: 👩🏻‍💻
-/* const getRandomNumber = (numMin, numMax) => {
+// 3 - Crear una función que genere un número random entre 1 y 900.
+// Puedes usar esta:
+const getRandomNumber = (numMin, numMax) => {
   return Math.floor(Math.random() * (numMax - numMin + 1) + numMin);
-}; */
+};
 
-// 4️⃣ - Asignar un número random al ID de los que serán nuestros pokemons
+// 4 - Asignar un número random al ID de los que serán nuestros pokemons
 // Declara 2 variables para cada pokemon y guarda los números que retorna la funci´øn en ellos
 
-// 🤓 Pista: algo como ... const poke1ID = getRandomNumber(1, 900);
+const poke1ID = getRandomNumber(1, 900);
+const poke2ID = getRandomNumber(1, 900);
 
-const obtenerNumeroAlAzar = (nMin, nMax) => {
-  return Math.floor(Math.random() * (nMax - nMin + 1) + nMin);
-}
-
-let pokemonID1 = obtenerNumeroAlAzar(1, 900);
-let pokemonID2 = obtenerNumeroAlAzar(1, 900);
-
-
-// 5️⃣ - Crear una función para traer (fetch) data de la API
+// 5 - Crear una función para traer (fetch) data de la API
 // Dale una mirada a la función fetch -> https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
 // Recuerda la URL de la API https://pokeapi.co/api/v2/pokemon/${pokeID}
 
-//Puedes usar esta: 👩🏻‍💻
-
-const traerPokemon = async (pokeID) => {
-  let url = `https://pokeapi.co/api/v2/pokemon/${pokeID}`;
-  console.log(url);
-  const response = await fetch(url);
+const getPokemon = async (pokeID) => {
+  const response = await fetch(` https://pokeapi.co/api/v2/pokemon/${pokeID}`);
   const data = await response.json();
   return data;
-}; 
+};
 
-// Open/Closed principle - Single Responsibility Principle
-
-
-// 6️⃣ - Vamos a crear los pokemons en la función createPokemons.
-// Primero Haz varias pruebas a las API para examinar bien qué devuelve, esa data
+// 6 - Crear los pokemons. Haz varias pruebas a las API para examinar bien qué devuelve, esa data
 // será necesaria para popular nuestros elementos HTML
-// 🤓 Pista: - Crea una función asíncrona que reciba los 2 ID de los pokemon, es decir los números que obtenemos de llamar la función random
-//           - Haz una llamada a la API por cada pokemon, guarda los datos que te devuelve en dos variables: pokemon1 y pokemon2
-//           - Toma los elementos HTML que seleccionamos más arriba y utiliza su propiendad innerHTML para añadir la info que necesitamos de la API
+// Crea una función asíncrona que reciba los 2 ID de los pokemon
+// Haz una llamada a la API por cada pokemon, guarda los datos que te devuelve en dos variables: pokemon1 y pokemon2
+// - Toma los elementos HTML que seleccionamos más arriba y utiliza su propiendad innerHTML para añadir la info que necesitamos de la API
 
+const createPokemons = async (poke1ID, poke2ID) => {
+  const pokemon1 = await getPokemon(poke1ID);
+  debugger;
+  poke1ImgElement.src = pokemon1.sprites["front_default"];
+  poke1NameElement.innerHTML += pokemon1.name;
+  poke1HpElement.innerHTML = +pokemon1.stats[0]["base_stat"];
+  poke1AttackElement.innerHTML += pokemon1.stats[1]["base_stat"];
+  poke1DefenseElement.innerHTML += pokemon1.stats[2]["base_stat"];
+  poke1TypeElement.innerHTML += pokemon1.types[0].type.name;
 
+  const pokemon2 = await getPokemon(poke2ID);
 
+  poke2ImgElement.src =
+    pokemon2.sprites.other["official-artwork"]["front_default"];
+  poke2NameElement.innerHTML += pokemon2.name;
+  poke2HpElement.innerHTML += pokemon2.stats[0]["base_stat"];
+  poke2AttackElement.innerHTML += pokemon2.stats[1]["base_stat"];
+  poke2DefenseElement.innerHTML += pokemon2.stats[2]["base_stat"];
+  poke2TypeElement.innerHTML += pokemon2.types[0].type.name;
+
+  console.log(pokemon1);
+};
+
+// BONUS 8 - Vamos a crear la función que permitirá que los pokemons interactúen y peleen
 
 // 🎁 Bonus! - Vamos a crear la función fightPokemons que permitirá que los pokemons interactúen y peleen
 
